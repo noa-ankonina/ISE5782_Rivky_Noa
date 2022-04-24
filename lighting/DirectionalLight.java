69d6d9f0ed169bed1404b8b5bd4a6ac9af0b5a81
RivkyShachar <92647408+RivkyShachar@users.noreply.github.com>
2022-04-24 14:43:22 +0300

    6.2 4

---
+++ lighting/DirectionalLight.java
@@ -0,0 +1,39 @@
+package lighting;
+
+import primitives.Color;
+import primitives.Point;
+import primitives.Vector;
+
+/**
+ * Class of directional light (like sun)
+ *
+ */
+public class DirectionalLight extends Light implements LightSource{
+
+    /**
+     * vector of direction of light
+     */
+    private Vector direction;
+
+
+
+    /**
+     * create the intensity and direction of the light
+     * @param intensity of the light
+     * @param direction of the light
+     */
+    public DirectionalLight(Color intensity, Vector direction) {
+        super(intensity);
+        this.direction=direction.normlize();
+    }
+
+    @Override
+    public Color getIntensity(Point p) {
+        return super.getIntensity();
+    }
+
+    @Override
+    public Vector getL(Point p) {
+        return direction;
+    }
+}
---
+++ lighting/SpotLight.java
@@ -0,0 +1,61 @@
+package lighting;
+
+import primitives.Color;
+import primitives.Point;
+import primitives.Vector;
+
+
+/**
+ * Class spot light
+ *
+ */
+public class SpotLight extends PointLight{
+
+    /**
+     * direction vector of the spot light
+     */
+    private Vector direction;
+
+    /**
+     * specular component (default=1, no affection on the sharp of the original spot)
+     */
+    private double specularN=1;
+
+    /**
+     * create the intensity, direction and position of the light
+     * @param intensity of the light
+     * @param direction of the light
+     * @param position of the light
+     */
+    public SpotLight(Color intensity, Vector direction, Point position) {
+        super(intensity,position);
+        this.direction=direction.normlize();
+    }
+
+    @Override
+    public Color getIntensity(Point p) {
+        double factor=Math.max(0,direction.dotProduct(getL(p)));
+        return super.getIntensity(p).scale(factor);
+    }
+
+    @Override
+    public Vector getL(Point p) {
+        return super.getL(p);
+    }
+
+    /**
+     * @param specularN specular component
+     * @return this spot light
+     */
+    public SpotLight setSpecularN(double specularN) {
+        this.specularN = specularN;
+        return this;
+    }
+
+    /**
+     * @return specularN
+     */
+    public double getSpecularN() {
+        return specularN;
+    }
+}
